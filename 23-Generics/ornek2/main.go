391c8259afa9b12baab4bbd46847db8c23c6d871
mustafacaglarkara <mustafacaglarkara@gmail.com>
2022-08-06 13:30:36 +0300

    Generic Eklendi Sayisal

--- 23-Generics/ornek2/main.go
+++ 23-Generics/ornek2/main.go
@@ -6,7 +6,15 @@ func Yazdir[T any](Deger T) {
 	fmt.Println(Deger)
 }
 
+func SayiArttir[age int64 | float32](myAge age) {
+	val := int(myAge) + 1
+	fmt.Println(val)
+}
+
 func main() {
 	var isim = "Mustafa Çağlar KARA"
+	var yas int64 = 38
 	Yazdir(isim)
+	Yazdir(yas)
+	SayiArttir(yas)
 }
